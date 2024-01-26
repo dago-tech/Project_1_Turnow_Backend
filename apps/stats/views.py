937afeb6a0569611b60c91f9b292b72ff108644e
danielAlex92 <daniel45675@gmail.com>
2024-01-25 19:24:42 -0500

    Improve logic in stats views

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,6 +1,4 @@
-from django.db.models import Avg, FloatField, ExpressionWrapper, F
-from django.db.models.functions import Coalesce, Cast
-from django.db.models import OuterRef, Subquery
+from django.db.models import Avg
 from rest_framework import generics, status
 from rest_framework.response import Response
 from ..turns.models import Turn, Desk
@@ -9,6 +7,9 @@ from .serializers import *
 
 
 class WaitingTimeByTurnView(generics.ListAPIView):
+    """
+    waiting_time and duration of turns filtered by start and end times
+    """
     serializer_class = TurnSerializer
 
     def get_queryset(self):
@@ -40,18 +41,27 @@ class WaitingTimeByTurnView(generics.ListAPIView):
 
 
 class WaitingTimeByDeskView(generics.ListAPIView):
+    """
+    Average of waiting_time and duration by Service Desk
+    """
     serializer_class = WaitingTimeByDeskSerializer
 
     def get_queryset(self):
-        # queryset = Desk.objects.annotate(
-        #     waiting_time_avg=Coalesce('turn__waiting_time', 0),
-        #     duration_avg=Coalesce('turn__duration', 0)
-        # )
 
+        start_date = self.request.query_params.get("start_date", None)
+        end_date = self.request.query_params.get("end_date", None)
+
+        turn_filtered = Turn.objects.filter(created__range=[start_date, end_date])
 
         queryset = Desk.objects.annotate(
             waiting_time_avg=Avg('turn__waiting_time'),
             duration_avg=Avg('turn__duration')
         )
-
-        return queryset
+        if queryset:
+            queryset = queryset.filter(turn__in=turn_filtered)
+            return queryset
+        else:
+            return Response(
+                {"message": "No turns or Service desks found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
